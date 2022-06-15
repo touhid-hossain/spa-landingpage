c976cfdf1429704fe5cb60b12a68cd8f03546c8c
unknown <mohmmedtouhid007@gmail.com>
2022-06-16 02:49:00 +0600

    initial commit

---
+++ main.js
@@ -0,0 +1,31 @@
+// Initiating Nav_menu
+const navBtn = document.getElementById("nav_btn")
+const navMenu = document.querySelector(".navbar");
+
+// Making_function
+ function menuOpen (e) {
+    navMenu.classList.toggle("navbar_menu-active")
+    navBtn.classList.toggle("navbar_burger-button_active")
+    console.log(e);
+}
+
+// Run the function
+navBtn.addEventListener('click', menuOpen);
+
+
+// Email Validation
+function emailValidate() {
+    var inputText = document.getElementsByTagName("input")[0].value;
+    var fetch = /\S+@\S+\.\S+/.test(inputText);
+  
+    if (fetch) {
+      document.getElementById("validationText").style.visibility = "hidden";
+      document.getElementById("email").style.border = '2px solid transparent';
+    } else {
+      document.getElementById("validationText").style.visibility = "visible";
+      document.getElementById("email").style.border =
+        "2px solid hsl(0, 94%, 66%)";
+    }
+  }
+
+
